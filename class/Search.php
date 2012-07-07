532811caac4c9735f104eff6b9c51ca08a18cc2b
Jumpsuitious <jumpsuitious@rocketmail.com>
2012-07-07 03:56:07 -0700

    RT search improvements
    Post deletes (not currently used) now work by setting the content
    to empty instead of by actually deleting the post from the index.
    
    For long posts, we now index the first MAX_SIZE_TO_INDEX characters
    instead of just discarding the entire post.
    
    Some refactoring.

--- class/Search.php
+++ class/Search.php
@@ -20,6 +20,27 @@ class Search
     return new PDO(SPHINXQL_DSN, SPHINXQL_USER, SPHINXQL_PASSWORD);
   }
 
+  /* Update post $id's body to be $body */
+  function _update_post($sphinx, $id, $body)
+  {
+    if (strlen($body) > MAX_SIZE_TO_INDEX) $body = substr($body, 0, MAX_SIZE_TO_INDEX);
+
+    // UPDATE doesn't work, so we load the post and then replace the body.
+    $id = intval($id);
+    $s = $sphinx->prepare("SELECT member_id, thread_id, date_posted FROM thread_post WHERE id=" . $id);
+    if (!$this->_exec($s, array(), "Failed to load post index")) return FALSE;
+    $post = $s->fetch(PDO::FETCH_ASSOC);
+    $post['id'] = $id;
+    $post['body'] = $body;
+
+    $s = $sphinx->prepare("REPLACE INTO thread_post (id, body, member_id, thread_id, date_posted) VALUES (:id, :body, :member_id, :thread_id, :date_posted)");
+    //print_r($post);
+    if (!$this->_exec($s, $post, "Failed to update post index")) return FALSE;
+
+    return TRUE;
+  }
+
+
   function query($query,$index,$offset=0)
   {
     $sphinx = $this->_connect();
@@ -46,15 +67,18 @@ class Search
 
   function thread_insert($data,$id)
   {
-    if (strlen($data['subject']) > MAX_SIZE_TO_INDEX) return TRUE; // Don't index
     $sphinx = $this->_connect();
     if (!$sphinx) return TRUE;
 
+    $subject = $data['subject'];
+    if (strlen($subject) > MAX_SIZE_TO_INDEX) $subject = substr($subject, 0, MAX_SIZE_TO_INDEX);
+
     $s = $sphinx->prepare("INSERT INTO thread (id, subject, member_id, date_posted) VALUES (?, ?, ?, ?)");
-    $q = array($id, $data['subject'], $data['member_id'], strtotime($data['date_posted']));
+    $q = array($id, $subject, $data['member_id'], strtotime($data['date_posted']));
     // Using strtotime() is a bit of a hack.  What we really want to do is
     // get UNIX_TIMESTAMP(CURRENT_TIMESTAMP) from postgres, but that's also
-    // a bit of a hack...
+    // a bit of a hack to do from here, as is doing it from Data and passing
+    // it here, so...
     //XXX: Watch out for timezone conversion?  (Not important just now since
     //     we only use timestamp for ordering, but still might want to check
     //     that we're doing the right thing.)
@@ -66,13 +90,15 @@ class Search
 
   function thread_post_insert($data,$id)
   {
-    if (strlen($data['body']) > MAX_SIZE_TO_INDEX) return TRUE; // Don't index
     $sphinx = $this->_connect();
     if (!$sphinx) return TRUE;
 
+    $body = $data['body'];
+    if (strlen($body) > MAX_SIZE_TO_INDEX) $body = substr($body, 0, MAX_SIZE_TO_INDEX);
+
     $s = $sphinx->prepare("INSERT INTO thread_post (id, body, member_id, thread_id, date_posted) VALUES (?, ?, ?, ?, ?)");
-    $q = array($id, $data['body'], $data['member_id'], $data['thread_id'], strtotime($data['date_posted']));
-    // (See notes on timestamp in thread_insert.)
+    $q = array($id, $body, $data['member_id'], $data['thread_id'], strtotime($data['date_posted']));
+    // (See notes on timestamp in thread_insert().)
 
     if (!$this->_exec($s, $q, "Failed to insert into post index")) return FALSE;
     return TRUE;
@@ -91,27 +117,9 @@ class Search
 
   function thread_post_update($data,$id)
   {
-    if (strlen($data['body']) > MAX_SIZE_TO_INDEX)
-    {
-      return $this->thread_post_delete($id);
-    }
-
     $sphinx = $this->_connect();
     if (!$sphinx) return TRUE;
-
-    $id = intval($id);
-    $s = $sphinx->prepare("SELECT member_id, thread_id, date_posted FROM thread_post WHERE id=" . $id);
-    if (!$this->_exec($s, array(), "Failed to load post index")) return FALSE;
-    $post = $s->fetch(PDO::FETCH_ASSOC);
-    $post['id'] = $id;
-    $post['body'] = $data['body'];
-
-    $s = $sphinx->prepare("REPLACE INTO thread_post (id, body, member_id, thread_id, date_posted) VALUES (?, ?, ?, ?, ?)");
-    $post = array($post['id'], $post['body'], $post['member_id'], $post['thread_id'], $post['date_posted']);
-    //print_r($post);
-    if (!$this->_exec($s, $post, "Failed to update post index")) return FALSE;
-
-    return TRUE;
+    return $this->_update_post($sphinx, $id, $data['body']);
   }
 
   function message_update($data) { return TRUE; }
@@ -126,13 +134,24 @@ class Search
 
   function thread_post_delete($id)
   {
+    //NOTE: I don't think is currently ever actually called, but unlike some
+    //      of the other functions, I think we can make a safe guess as to
+    //      what it should do.
+
     $sphinx = $this->_connect();
     if (!$sphinx) return TRUE;
 
+    // Rather than actually DELETE the record and leave a hole, let's just
+    // clear out the content.
+    return $this->_update_post($sphinx, $id, '');
+
+    /*
+    (Old code that actually deletes the record and leaves a hole.)
     $id = intval($id);
     $s = $sphinx->prepare("DELETE FROM thread_post WHERE id=" . $id);
     if (!$this->_exec($s, "Failed to delete from post index")) return FALSE;
     return TRUE;
+    */
   }
 
   function message_delete($id) { return TRUE; }
